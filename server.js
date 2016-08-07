cc3590156fb851039e322dbdbb65a92916411fe2
aziza alhouni <azizaalhouni@yahoo.com>
2016-08-06 23:32:33 -0700

    package.json

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
+
 var express = require('express'),
- 	requestProxy = require('express-request-proxy'),
- 	port = process.env.PORT || 4000,
- 	app = express();
+  requestProxy = require('express-request-proxy'),
+  port = process.env.PORT || 4000,
+  app = express();
 
- var proxyEve = function(request, response) {
- 	console.log('Routing EVE request for ', request.params[0]);
- 	(
- 		requestProxy({
- 			url: 'http://api.eve-central.com/' + request.params[0],
- 		})
-  	)(request, response);
-  };
+var proxyEve = function(request, response) {
+  console.log('Routing EVE request for ', request.params[0]);
+  (
+		requestProxy({
+  url: 'http://api.eve-central.com/' + request.params[0],
+		})
+	)(request, response);
+};
 
- app.get('/eve-central/*', proxyEve);
+app.get('/eve-central/*', proxyEve);
 
-  app.use(express.static('./'));
+app.use(express.static('./public'));
 
- app.get('*', function(request, response) {
- 	console.log('New request: ', request.url);
- 	response.sendFile('index.html', {root: '.'});
- });
+app.get('*', function(request, response) {
+  console.log('New request: ', request.url);
+  response.sendFile('index.html', {root: './public'});
+});
 
- app.listen(port, function() {
- 	console.log('Server started on port ' + port + '!');
- });
+app.listen(port, function() {
+  console.log('Server started on port ' + port + '!');
+});
